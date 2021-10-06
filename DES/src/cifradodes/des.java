560bd3360a57fd105bf03e95941cf1a66d1cb3dc
mauxar <maguirreg1900@alumno.ipn.mx>
2021-10-05 21:41:08 -0500

    des

---
+++ DES/src/cifradodes/des.java
@@ -0,0 +1,18 @@
+
+package cifradodes;
+
+/**
+ *
+ * @author mauag
+ */
+public class des {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        menu ci = new menu();
+        ci.setVisible(true);
+    }
+}
